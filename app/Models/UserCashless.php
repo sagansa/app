de0b45013565adb2b011e5a5a70318bc2c1d0d61
sagansa <asapanganbangsa@gmail.com>
2022-08-21 22:48:39 +0700

    update

--- app/Models/UserCashless.php
+++ app/Models/UserCashless.php
@@ -59,6 +59,6 @@ class UserCashless extends Model
 
     public function getUserCashlessNameAttribute()
     {
-        return $this->adminCashless->cashlessProvider->name . ' - ' . $this->store->nickname . ' - ' . $this->email;
+        return $this->adminCashless->cashlessProvider->name . ' - ' . $this->store->nickname;
     }
 }
