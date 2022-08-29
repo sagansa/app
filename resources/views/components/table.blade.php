398d461bc983dab0eb71d35346afa243031a52fc
sagansa <asapanganbangsa@gmail.com>
2022-08-29 11:36:08 +0700

    update

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -1,4 +1,4 @@
-<div class="min-w-full overflow-hidden overflow-x-auto align-middle shadow sm:rounded-lg">
+{{-- <div class="min-w-full overflow-hidden overflow-x-auto align-middle shadow sm:rounded-lg">
     <table class="min-w-full divide-y divide-cool-gray-200">
         <thead>
             <tr>
@@ -12,4 +12,19 @@
             {{ $foot }}
         </tfoot>
     </table>
-</div>
+</div> --}}
+
+
+<table class="min-w-full border-separate" style="border-spacing: 0">
+    <thead>
+        <tr>
+            {{ $head }}
+        </tr>
+    </thead>
+    <tbody wire:loading.class="text-muted" class="bg-white divide-y divide-cool-gray-200">
+        {{ $body }}
+    </tbody>
+    <tfoot>
+        {{ $foot }}
+    </tfoot>
+</table>
--- resources/views/components/tables/card.blade.php
+++ resources/views/components/tables/card.blade.php
@@ -1,1 +1,11 @@
-<div class="overflow-hidden mt-5 -mx-4 ring-1 ring-gray-300 sm:-mx-6 md:mx-0 md:rounded-lg">{{ $slot }}</div>
+{{-- <div class="overflow-hidden mt-5 -mx-4 ring-1 ring-gray-300 sm:-mx-6 md:mx-0 md:rounded-lg">{{ $slot }}</div> --}}
+
+<div class="mt-8 flex flex-col">
+    <div class="-my-2 -mx-4 sm:-mx-6 lg:-mx-8">
+        <div class="inline-block min-w-full py-2 align-middle">
+            <div class="shadow-sm ring-1 ring-black ring-opacity-5">
+                {{ $slot }}
+            </div>
+        </div>
+    </div>
+</div>
